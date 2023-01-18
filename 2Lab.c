2db3f14518e2b23f60b4437adcdc81550ab64423
tarasov90 <49621472+tarasov90@users.noreply.github.com>
2023-01-18 23:13:59 +0300

    Add files via upload

---
+++ 2Lab.c
@@ -0,0 +1,18 @@
+﻿
+#include <stdio.h>
+
+int main()
+{
+	int a = 0x15;
+	printf("%d\n",a);
+	printf("%f\n", a);
+	printf("%li\n", a);
+	int i = -1;
+	printf("%d\n", i);
+	printf("%u\n", i);
+
+	double n = 0.1234;
+	printf("%d\n", n);
+	printf("%u\n", n);
+	return 0;
+}
