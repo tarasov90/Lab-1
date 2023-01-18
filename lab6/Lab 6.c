f90533dfcebcf8dd393d513a074ec4fa53159b8d
tarasov90 <49621472+tarasov90@users.noreply.github.com>
2023-01-18 23:20:08 +0300

    Add files via upload

---
+++ lab6/Lab 6.c
@@ -0,0 +1,22 @@
+﻿
+#include <stdio.h>
+#define arraySize 5
+//const int arraySizeByConst = 5;
+int main()
+{
+	
+	int array1[5] = { 1,2,3,4,5 };
+	int array2[] = {1,2,3,4,5};
+	int array3[arraySize];
+	//int array4[arraySizeByConst];
+	for (int i = 0; i < sizeof(array1)/sizeof(int); i++)
+	{
+		printf("array1[%i] = %d \n", i, array1[i]);
+	}
+	char string[] = "Hello, World! \n";
+	char string2[50];
+	printf(string);
+	scanf("%s",string2);
+	printf(string2);
+
+}
